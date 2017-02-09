a802395558e58b8b11a582c524def5c5c6d3c288
Wei Hanwei <weihanwei@jd.com>
2017-02-09 15:30:04 +0800

    update: signin and signup

---
+++ public/js/signup.js
@@ -0,0 +1,20 @@
+/**
+ * Created by weihanwei on 2017/2/6.
+ */
+$(function(){
+  $('#submit').click(function(event){
+    event.preventDefault();
+    var formdata = new FormData($('#infoForm')[0]);
+    $.ajax({
+      url: '/signup',
+      type: 'post',
+      data: formdata,
+      processData : false,
+      contentType : false,
+      dataType: 'text',
+      success: function(str){
+        console.log(str);
+      }
+    });
+  });
+});
